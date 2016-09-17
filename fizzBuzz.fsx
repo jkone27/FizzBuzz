dcdec279ca78e3fad13c327add888f4f6e3998fe
Jkone27 <jkone27@users.noreply.github.com>
2016-09-17 23:05:12 +0200

    added printFizzBuzz function

--- fizzBuzz.fsx
+++ fizzBuzz.fsx
@@ -1,7 +1,7 @@
-
 open System
-(* giacomo parmigiani *)
 open FSharp.Core
+
+type FB = | Fizz | Buzz | FizzBuzz
 
 let fizz x = 
     x % 3 = 0
@@ -11,10 +11,26 @@ let buzz x =
 
 let testFizzBuzz x = 
     match x with
-    | _ when fizz x && buzz x -> "fizzBuzz"
-    | _ when buzz x -> "buzz"
-    | _ when fizz x -> "fizz"
-    | _ -> x.ToString()
+    | _ when fizz x && buzz x -> Some(FizzBuzz)
+    | _ when buzz x -> Some(Buzz)
+    | _ when fizz x -> Some(Fizz)
+    | _ -> None
+
+let fizzBuzzString x =
+    match x with
+        | Fizz -> "fizz"
+        | Buzz -> "buzz"
+        | _ -> "FizzBuzz"
+
+let printFizzBuzz x =
+    let result = 
+        match (testFizzBuzz x) with 
+        |Some(y) -> fizzBuzzString y
+        |_ -> x.ToString()
+    printfn "%d : %s" x result
+
+//some "basic" unit tests
+[0;1;3;5;15] |> Seq.iter printFizzBuzz
 
 [1..100] 
-|> Seq.iter (fun n -> printfn "%s" (testFizzBuzz n))
+|> Seq.iter printFizzBuzz
